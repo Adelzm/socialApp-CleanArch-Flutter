27bc94c9f1ddc1049509ed0781cd651cce98465d
Adel <adel.mouhandes@gmail.com>
2022-12-23 11:56:53 +0100

    - implemente CRUD for LocalFeedDatasource

---
+++ lib/src/features/feed/data/datasources/local_feed_datasource.dart
@@ -0,0 +1,36 @@
+import 'package:hive/hive.dart';
+import 'package:social_media_clean_archi/src/shared/data/models/post_model.dart';
+import 'package:social_media_clean_archi/src/shared/domain/entities/post_entity.dart';
+
+abstract class LocalFeedDatasource {
+  Future<void> addPosts(PostEntity post);
+  Future<void> deleteAllPosts();
+  Future<List<PostEntity>> getPosts();
+}
+
+class LocalFeedDatasourceImp implements LocalFeedDatasource {
+  String boxName = 'posts';
+  Type boxType = PostModel;
+
+  @override
+  Future<void> addPosts(PostEntity post) async {
+    Box box = await _openBox();
+    await box.put(post.id, PostModel.fromEntity(post));
+  }
+
+  @override
+  Future<void> deleteAllPosts() async {
+    Box box = await _openBox();
+    await box.clear();
+  }
+
+  @override
+  Future<List<PostEntity>> getPosts() async {
+    Box<PostModel> box = await _openBox() as Box<PostModel>;
+    return box.values.toList().map((post) => post.toEntity()).toList();
+  }
+
+  Future<Box> _openBox() async {
+    return Hive.openBox<PostModel>(boxName);
+  }
+}
